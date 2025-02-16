7adc301f6c6a9b02e654314d6fafcb428b804d68
Arcti <stelland1234@gmail.com>
2025-02-16 19:51:14 +0100

    Cleaned up and fixed some bugs and added some overall polish

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get ( '/', (_, res) => res.sendFile(import.meta.dirname + "/index.html"));
 app.post( '/', StartGame);
 app.all ( '/teapot', (_, response) => response.sendStatus(418) );
 app.use ( (_, response) => response.sendStatus(404) );
-app.listen(80);
+app.listen(3000);
 
 // Generate new game from input
 function StartGame(request, response) {
@@ -22,19 +22,22 @@ function StartGame(request, response) {
     let lobbyID = 1;
 	lobbyBytes.forEach((byte) => { lobbyID *= byte });
     const rng = new Random(lobbyID);
-    lobbyBytes = lobbyBytes.map((_) => { return rng.int(48, 122) });
+    lobbyBytes = new Uint8Array(8);
+	lobbyBytes = lobbyBytes.map((_) => { return rng.int(48, 122) });
     const spy = rng.int(1, contestants);
     const location = Object.keys(locations).at(rng.int(0, Object.keys(locations).length - 1));
 	const locationLength = Object.values(locations[location]).length;
 	let roleID = rng.int(0, locationLength - 1) + player;
 	while (roleID >= locationLength) { roleID -= locationLength };
     const role = Object.values(locations[location]).at(roleID);
-	console.log(`${location}: ${role} (Spy: ${spy})`);
+    const datetime = new Date(Date.now());
+	console.log(`${request.ip} [${datetime.toLocaleDateString()} ${datetime.toLocaleTimeString()}]: ${location}: ${role} (Spy: ${spy})`);
     
     return response.json({
     	"lobby": String.fromCharCode.apply(null, lobbyBytes),
     	"location": spy == player ? "Unknown" : location,
     	"role": spy == player ? "Spy" : role,
-    	"locations": Object.keys(locations)
+    	"locations": Object.keys(locations),
+    	"game": new String(lobbyID).slice(0, 5)
     });
 }
